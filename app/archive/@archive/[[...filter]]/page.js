6190ccc4ba0fefb2085f14bafe3f829818f747e6
KubaJarmolowicz <kuba.jarmolowicz92@gmail.com>
2025-06-14 10:34:58 +0200

    Add catch-all route + error handling

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { NewsList } from "@/components/Organisms";
+import {
+  getNewsForYear,
+  getAvailableNewsYears,
+  getNewsForYearAndMonth,
+  getAvailableNewsMonths,
+} from "@/lib/news";
+
+const FilteredNewsPage = async ({ params }) => {
+  const { filter } = await params;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (Array.isArray(news) && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter.");
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+};
+
+export default FilteredNewsPage;
